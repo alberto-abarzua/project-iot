a442f1cc537e854de792562fecb811908e411c8b
alberto-abarzua <uintuser@pm.me>
2023-06-04 20:15:44 -0400

    fix: connect timeout

--- rasp/src/ble/utils.py
+++ rasp/src/ble/utils.py
@@ -105,7 +105,8 @@ class Connecting:
             adapter.start()
             time.sleep(2)  # Give the adapter some time to start
             try:
-                connected_device = adapter.connect(self.context.device_mac, address_type=pygatt.BLEAddressType.public)
+                connected_device = adapter.connect(self.context.device_mac, address_type=pygatt.BLEAddressType.public,
+                                                   timeout = 10, auto_reconnect=True)
                 print("Connected!")
                 return connected_device
             except Exception as e:
