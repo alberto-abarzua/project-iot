c439e8881eea57d6a72335132b7e4b6f1d1dd605
alberto-abarzua <uintuser@pm.me>
2023-07-10 13:38:53 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -52,11 +52,9 @@ rungui_rasp)
     docker compose up -d
     cd ./rasp/
     # create venv
-    python3 -m venv venv
     # activate venv
     ./venv/bin/activate
-    pip install -r requirements.txt
-    python3 src/gui.py
+    python ./src/gui.py
 
     docker compose down --remove-orphans
     ;;
